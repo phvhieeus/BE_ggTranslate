efe1a4dc7b04ad6180837856a585502a3fa5855b
phvhieeus <hieuvanpham1211@gmail.com>
2025-01-14 12:40:03 +0700

    Update User and Add Lombok

--- src/main/java/com/translate/domain/User.java
+++ src/main/java/com/translate/domain/User.java
@@ -2,11 +2,15 @@ package com.translate.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +23,9 @@ public class User {
     @NotBlank(message = "Password cannot be blank")
     private String password;
 
+    @NotBlank(message = "Phone cannot be blank")
+    private double phone;
+
     private int age;
 
     private String address;
@@ -28,86 +35,6 @@ public class User {
     private String createdBy;
     private String updatedBy;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public @NotBlank(message = "Email cannot be blank") String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@NotBlank(message = "Email cannot be blank") String email) {
-        this.email = email;
-    }
-
-    public @NotBlank(message = "Password cannot be blank") String getPassword() {
-        return password;
-    }
-
-    public void setPassword(@NotBlank(message = "Password cannot be blank") String password) {
-        this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
     @Override
     public String toString() {
         return "User{" +
@@ -115,6 +42,7 @@ public class User {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", phone=" + phone +
                 ", age=" + age +
                 ", address='" + address + '\'' +
                 ", createdAt=" + createdAt +
